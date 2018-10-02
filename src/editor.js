557dae5dc7885f257c7b27f30089a5d782c05b90
Fabian Vilers <fabian.vilers@dev-one.com>
2018-10-02 15:01:15 +0200

    Add style

--- src/editor.js
+++ src/editor.js
@@ -1,4 +1,7 @@
 export const transformToEditor = editor => {
   // Indicate that the element is editable
   editor.setAttribute('contentEditable', true);
+
+  // Add a custom class
+  editor.className = '__editor';
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { transformToEditor } from './editor';
+import './style.css';
 
 (function() {
   document.querySelectorAll('[data-tiny-editor]').forEach(transformToEditor);
