b1c05457cf51535e6cfe46fb418d621d1167611d
Fabian Vilers <fabian.vilers@dev-one.com>
2018-10-02 15:03:38 +0200

    Add the toolbar

--- src/editor.js
+++ src/editor.js
@@ -1,7 +1,13 @@
+import { createToolbar } from './toolbar';
+
 export const transformToEditor = editor => {
   // Indicate that the element is editable
   editor.setAttribute('contentEditable', true);
 
   // Add a custom class
   editor.className = '__editor';
+
+  // Create a toolbar
+  const toolbar = createToolbar();
+  editor.insertAdjacentElement('beforebegin', toolbar);
 };
---
+++ src/toolbar.js
@@ -0,0 +1,6 @@
+export const createToolbar = () => {
+  const toolbar = document.createElement('div');
+  toolbar.className = '__toolbar';
+
+  return toolbar;
+};
