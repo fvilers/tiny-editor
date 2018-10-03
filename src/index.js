28bc489a7ca824d905148263ed2fb50ff68dd801
Fabian Vilers <fabian.vilers@dev-one.com>
2018-10-03 10:29:30 +0200

    Import style in babel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { transformToEditor } from './editor';
-
-// TODO: this don't work with rollup, move CSS elsewhere
-// import './style.css';
+import './style.css';
 
 (function() {
   document.querySelectorAll('[data-tiny-editor]').forEach(transformToEditor);
