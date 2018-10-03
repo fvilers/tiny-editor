3206df01fa2e9bba8cf7edc52e80af7cecc855e6
Fabian Vilers <fabian.vilers@dev-one.com>
2018-10-03 10:15:00 +0200

    Add babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+const babel = require('rollup-plugin-babel');
+
 module.exports = {
   input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'iife'
-  }
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
 };
