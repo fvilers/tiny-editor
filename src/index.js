0f69e68847b7ef6d50a42d24e81b066138ac921c
syonfox <contact@kierlindsay.ca>
2022-01-19 19:46:54 -0800

    [200~window.transformToEditor = transformToEditor;

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import { transformToEditor } from './editor';
 import './style.css';
 
 document.querySelectorAll('[data-tiny-editor]').forEach(transformToEditor);
+window.transformToEditor = transformToEditor;
