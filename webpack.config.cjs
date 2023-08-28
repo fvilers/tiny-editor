326aca035e06d76520a1627e479014ee47795327
jcable <julian.cable@bbc.co.uk>
2023-08-28 08:11:03 +0100

    fix: remove rollup

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
